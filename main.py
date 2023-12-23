5a2f6a1993dd0dfe6f9f84f0392577d4f2ae034a
ihseh <hmuntiu@gmail.com>
2023-12-22 19:00:33 -0500

    created game window

---
+++ main.py
@@ -0,0 +1,32 @@
+import arcade
+import arcade.gui
+
+SCREEN_WIDTH = 800
+SCREEN_HEIGHT = 800
+
+class GameView(arcade.View):
+
+    def __init__(self):
+        super().__init__()
+        arcade.set_background_color(arcade.color.WHITE)
+
+
+    def on_draw(self):
+        pass
+
+    def on_update(self, delta_time):
+        pass
+
+
+    def on_key_press(self, key, key_modifiers):
+        pass
+
+
+window = arcade.Window(SCREEN_WIDTH, SCREEN_HEIGHT)
+
+def main():
+    gameView = GameView()
+    window.show_view(gameView)
+    arcade.run()
+
+main()
