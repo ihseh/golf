4b5426534ea4007050e6973805ba6e12cb6956d4
ihseh <hmuntiu@gmail.com>
2023-12-23 18:41:37 -0500

    rotation with wheel hitboxes

--- race.py
+++ race.py
@@ -1,14 +1,16 @@
 import arcade
 import arcade.gui
+import math
 
 SCREEN_WIDTH = 1300
 SCREEN_HEIGHT = 900
-BIKE_SCALE = 0.8
+BIKE_SCALE = 0.5
+
 
 class Bike():
     def __init__(self):
-        self.x = 300
-        self.y = 300
+        self.x = 600
+        self.y = 500
         self.sprite = arcade.Sprite("sprites/player.png", center_x = self.x, center_y = self.y, scale = BIKE_SCALE)
         self.backWheel = Wheel("back",self.x,self.y)
         self.frontWheel = Wheel("front",self.x,self.y)
@@ -39,16 +41,22 @@ class GameView(arcade.View):
     def on_draw(self):
         arcade.start_render()
         self.clear()
-        self.bike.sprite.draw()
-        arcade.draw_circle_filled(center_x = self.bike.backWheel.x, center_y = self.bike.backWheel.y, radius = BIKE_SCALE * 75, color = (0,0,255,150))
-        arcade.draw_circle_filled(center_x = self.bike.frontWheel.x, center_y = self.bike.frontWheel.y, radius = BIKE_SCALE * 75, color = (0,0,255,150))
-        #center of bike: arcade.draw_circle_filled(center_x = self.bike.x, center_y = self.bike.y, radius = BIKE_SCALE * 30, color = (0,0,255))
+        self.bike.sprite.draw() #draw bike
+        #draw wheels
+        backX = self.bike.x + math.sqrt(((BIKE_SCALE*165)**2) *2) * math.sin(math.radians(self.bike.sprite.angle)-math.pi/4)
+        backY = self.bike.y - math.sqrt(((BIKE_SCALE*165)**2) *2) * math.cos(math.radians(self.bike.sprite.angle)-math.pi/4)
+        frontX = self.bike.x + math.sqrt(((BIKE_SCALE*165)**2) *2) * math.cos(math.radians(self.bike.sprite.angle)-math.pi/4)
+        frontY = self.bike.y + math.sqrt(((BIKE_SCALE*165)**2) *2) * math.sin(math.radians(self.bike.sprite.angle)-math.pi/4)
+        arcade.draw_circle_filled(center_x = backX, center_y = backY, radius = BIKE_SCALE * 75, color = (0,0,255,150)) #back wheel hitbox
+        arcade.draw_circle_filled(center_x = frontX, center_y = frontY, radius = BIKE_SCALE * 75, color = (0,0,255,150)) #front wheel hitbox
     
     def on_update(self, delta_time):
         if self.spinLeft:
             self.bike.sprite.angle -= 5
         elif self.spinRight:
             self.bike.sprite.angle += 5
+
+        
 
     def on_key_press(self, key, key_modifiers):
         if key == arcade.key.LEFT:
